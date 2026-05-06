9a5d4c6e50051e10280e79705ee3a70e1a76a359
joao-pedro2225 <joao.9532588@aluno.mg.gov.br>
2026-05-06 14:13:38 +0000

    correção

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
-//=== ESTADO (dados de aplicação) ===
+//=== ESTADO (dados da aplicação) === 
 
 let likeCount = 0;
 let dislikeCount = 0;
 let curtido = false;
 let descurtido = false;
 
-//=== SERVICE (regras de negócio) ===
+//=== SERVICE (regras de negócio) === 
 
 function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
 
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
   }
 
 }
@@ -32,32 +28,41 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
   }
+}
+
+// === VIEW (interface/renderização)===
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
 }
 
 //=== CONTROLLER (intermediação)===
 
 function clicarCurtir(){
   curtir();
+  atualizarTela();
 }
-function clicardescurtir(){
+function clicarDescurtir(){
   descurtir();
+  atualizarTela();
 }
 
 // === EVENTOS ===
 
 document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
-document.getElementById("dislikeBtn").addEventListener("click", clicardescurtir);
+document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO ===  
+
+atualizarTela();
